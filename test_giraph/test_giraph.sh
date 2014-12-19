0fa490c32500954cea0f6ca2ad693a435ae98bc1
alee-altiscale <alee@altiscale.com>
2014-12-19 14:51:03 -0800

    [AE-926] Added giraph test cases

--- test_giraph/test_giraph.sh
+++ test_giraph/test_giraph.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+#!/bin/bash -x
 
-GIRAPH_HOME=/opt/giraph
+GIRAPH_HOME=/opt/giraph/
 giraph_version=1.1.0
 user_name=alti-test-01
 test_hdfs_dir=/user/$user_name/test_giraph/
 
 hadoop_classpath=$HADOOP_CLASSPATH
 
-for jar in `find $GIRAPH_HOME -type f -name "*.jar" | sort`
+for j in `find $GIRAPH_HOME -type f -name "*.jar" | sort`
 do
-  hadoop_classpath=$hadoop_classpath:$jar
+  hadoop_classpath=$hadoop_classpath:$j
 done
 
-for jar in `find $GIRAPH_HOME -type f -name "*.jar" | sort`
+for j in `find $GIRAPH_HOME -type f -name "*.jar" | sort`
 do
-  hadoop_libjars=$hadoop_libjars,$jar
+  hadoop_libjars=$hadoop_libjars,$j
 done
 
 hdfs dfs -mkdir -p $test_hdfs_dir/test0/
@@ -26,7 +26,8 @@ export HADOOP_CLASSPATH=$hadoop_classpath
 
 # Test Case 0
 
-$HADOOP_HOME/bin/hadoop jar $GIRAPH_HOME/giraph-core-${giraph_version}.jar org.apache.giraph.GiraphRunner -libjars $hadoop_libjars org.apache.giraph.examples.SimpleShortestPathsComputation -vif org.apache.giraph.io.formats.JsonLongDoubleFloatDoubleVertexInputFormat -vip $test_hdfs_dir/test0/tiny_graph.txt -vof org.apache.giraph.io.formats.IdWithValueTextOutputFormat -op $test_hdfs_dir/test0/shortestpaths -w 1
+# $HADOOP_HOME/bin/hadoop jar $GIRAPH_HOME/giraph-core-${giraph_version}.jar org.apache.giraph.GiraphRunner -libjars $hadoop_libjars org.apache.giraph.examples.SimpleShortestPathsComputation -vif org.apache.giraph.io.formats.JsonLongDoubleFloatDoubleVertexInputFormat -vip $test_hdfs_dir/test0/tiny_graph.txt -vof org.apache.giraph.io.formats.IdWithValueTextOutputFormat -op $test_hdfs_dir/test0/shortestpaths -yj $hadoop_libjars -w 1
+$GIRAPH_HOME/bin/giraph $GIRAPH_HOME/giraph-examples-${giraph_version}.jar org.apache.giraph.examples.SimpleShortestPathsComputation -vif org.apache.giraph.io.formats.JsonLongDoubleFloatDoubleVertexInputFormat -vip $test_hdfs_dir/test0/tiny_graph.txt -vof org.apache.giraph.io.formats.IdWithValueTextOutputFormat  -op $test_hdfs_dir/test0/shortestpaths -yj $hadoop_libjars -w 1
 
 # Add other test case if applicable
 
